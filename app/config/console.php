3b3fd59a2f4447f56112a3cc3dcd94e235810695
Philipp Frenzel <philipp@frenzel.net>
2013-05-05 15:10:56 +0200

    adding console configuration
    see sub

---
+++ app/config/console.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'id' => 'frenzelgmbhconsole',
+	'basePath' => dirname(__DIR__),
+	'controllerPath' => '@yii/console/controllers',
+	'components' => array(
+		'db' => array(
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=rps',
+			'username' => 'root',
+			'password' => '',
+		),
+	),
+	'params' => array(
+		'adminEmail' => 'philipp@frenzel.net',
+	),
+);

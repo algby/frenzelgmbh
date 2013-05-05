d4d971a604a31510923e4a8373128793cfa5ae96
Philipp Frenzel <philipp@frenzel.net>
2013-05-05 15:11:11 +0200

    adding user table
    adding user table migration

---
+++ app/migrations/m130505_131012_users.php
@@ -0,0 +1,15 @@
+<?php
+
+class m130505_131012_users extends \yii\db\Migration
+{
+	public function up()
+	{
+
+	}
+
+	public function down()
+	{
+		echo "m130505_131012_users cannot be reverted.\n";
+		return false;
+	}
+}

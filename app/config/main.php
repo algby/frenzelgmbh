c93b9d0022462dcedf8e5cd09d8f0d2471b8845a
Philipp Frenzel <philipp@frenzel.net>
2013-05-05 14:02:07 +0200

    adding database connection string

--- app/config/main.php
+++ app/config/main.php
@@ -3,9 +3,16 @@
 return array(
 	'id' => 'frenzelgmbh',
 	'basePath' => dirname(__DIR__),
+	'name' => 'Frenzel GmbH - Software Entwicklung, QlikView Consulting, Unternehmensplanung',
 	'components' => array(
 		'cache' => array(
 			'class' => 'yii\caching\FileCache',
+		),
+		'db' => array(
+			'class' => 'yii\db\Connection',
+			'dsn' => 'mysql:host=localhost;dbname=rps',
+			'username' => 'root',
+			'password' => '',
 		),
 		'user' => array(
 			'class' => 'yii\web\User',

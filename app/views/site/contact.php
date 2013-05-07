2dd0efc1c7c586fc8c6313eca7b59528e8a38ea4
Philipp Frenzel <philipp@frenzel.net>
2013-05-07 09:48:42 +0200

    enhancing look n feel

--- app/views/site/contact.php
+++ app/views/site/contact.php
@@ -7,27 +7,30 @@ use yii\helpers\Html;
  */
 $this->title = 'Contact';
 ?>
-<h1><?php echo Html::encode($this->title); ?></h1>
 
-<?php if(Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-<div class="alert alert-success">
-	Thank you for contacting us. We will respond to you as soon as possible.
-</div>
-<?php return; endif; ?>
+<section id="content">
+	<h1><?php echo Html::encode($this->title); ?></h1>
 
-<p>
-	If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-</p>
-
-<?php $form = $this->beginWidget('yii\widgets\ActiveForm', array(
-	'options' => array('class' => 'form-horizontal'),
-	'fieldConfig' => array('inputOptions' => array('class' => 'input-xlarge')),
-)); ?>
-	<?php echo $form->field($model, 'name')->textInput(); ?>
-	<?php echo $form->field($model, 'email')->textInput(); ?>
-	<?php echo $form->field($model, 'subject')->textInput(); ?>
-	<?php echo $form->field($model, 'body')->textArea(array('rows' => 6)); ?>
-	<div class="form-actions">
-		<?php echo Html::submitButton('Submit', null, null, array('class' => 'btn btn-primary')); ?>
+	<?php if(Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+	<div class="alert alert-success">
+		Thank you for contacting us. We will respond to you as soon as possible.
 	</div>
-<?php $this->endWidget(); ?>
+	<?php return; endif; ?>
+
+	<p>
+		If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+	</p>
+
+	<?php $form = $this->beginWidget('yii\widgets\ActiveForm', array(
+		'options' => array('class' => 'form-horizontal'),
+		'fieldConfig' => array('inputOptions' => array('class' => 'input-xlarge')),
+	)); ?>
+		<?php echo $form->field($model, 'name')->textInput(); ?>
+		<?php echo $form->field($model, 'email')->textInput(); ?>
+		<?php echo $form->field($model, 'subject')->textInput(); ?>
+		<?php echo $form->field($model, 'body')->textArea(array('rows' => 6)); ?>
+		<div class="form-actions">
+			<?php echo Html::submitButton('Submit', null, null, array('class' => 'button bg-color-blue fg-color-white')); ?>
+		</div>
+	<?php $this->endWidget(); ?>
+</section>
--- app/views/site/login.php
+++ app/views/site/login.php
@@ -7,15 +7,18 @@ use yii\helpers\Html;
  */
 $this->title = 'Login';
 ?>
-<h1><i class="icon-key"></i> <?php echo Html::encode($this->title); ?></h1>
 
-<p>Please fill out the following fields to login:</p>
+<section id="content">
+	<h1><i class="icon-key"></i> <?php echo Html::encode($this->title); ?></h1>
 
-<?php $form = $this->beginWidget('yii\widgets\ActiveForm', array('options' => array('class' => 'form-horizontal'))); ?>
-	<?php echo $form->field($model, 'username')->textInput(); ?>
-	<?php echo $form->field($model, 'password')->passwordInput(); ?>
-	<?php echo $form->field($model, 'rememberMe')->checkbox(); ?>
-	<div class="form-actions">
-		<?php echo Html::submitButton('Login', null, null, array('class' => 'btn btn-primary')); ?>
-	</div>
-<?php $this->endWidget(); ?>
+	<p>Please fill out the following fields to login:</p>
+
+	<?php $form = $this->beginWidget('yii\widgets\ActiveForm', array('options' => array('class' => 'form-horizontal'))); ?>
+		<?php echo $form->field($model, 'username')->textInput(); ?>
+		<?php echo $form->field($model, 'password')->passwordInput(); ?>
+		<?php echo $form->field($model, 'rememberMe')->checkbox(); ?>
+		<div class="form-actions">
+			<?php echo Html::submitButton('Login', null, null, array('class' => 'button bg-color-blue fg-color-white')); ?>
+		</div>
+	<?php $this->endWidget(); ?>
+</section>

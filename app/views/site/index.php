bac7980bb3588624ec213d09c43b12b3f9c243ef
Philipp Frenzel <philipp@frenzel.net>
2013-05-07 10:06:56 +0200

    adding tiles and changing layout

--- app/views/site/index.php
+++ app/views/site/index.php
@@ -5,16 +5,37 @@
 $this->title = 'Welcome';
 ?>
 
-<div class="jumbotron bg-color-blueLight">
-	<h1>Welcome!</h1>
-
-	<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus
-		commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-	<a class="button bg-color-green fg-color-white big" href="http://www.yiiframework.com">Get started with Yii<i class="icon-arrow-right-3 right"></i></a>
-</div>
-
-<!-- Example row of columns -->
 <section id="content">
+
+	<div class="row-fluid">
+		<div class="tile double-vertical bg-color-yellow icon">
+		    <div class="tile-content">
+		    	<i class="icon-arrow-right-3"></i>
+		    </div>
+		    <div class="brand">
+		    		<span class="name">Frenzel GmbH</span>
+		    </div>
+	    </div>
+	    <div class="tile double bg-color-blue">
+		    <div class="tile-content">
+				<a class="button bg-color-green fg-color-white big" href="http://www.yiiframework.com">Get started with Yii2<i class="icon-arrow-right-3 right"></i></a>
+		    </div>
+		    <div class="brand">
+		    	<span class="name">Yii2 Framework</span>
+		    </div>
+	    </div>
+	</div>
+
+	<div class="jumbotron bg-color-blueLight">
+		<h1>Welcome!</h1>
+
+		<p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus
+			commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		
+	</div>
+
+	<!-- Example row of columns -->
+
 	<div class="row-fluid">
 		<div class="span4">
 			<h2>Heading</h2>

478cb67faca3adf939395c6a78add5698ceff02b
Philipp Frenzel <philipp@frenzel.net>
2013-05-08 07:34:40 +0200

    renaming yii_ext to yiiext to go with new yii2 names...

--- web/index.php
+++ web/index.php
@@ -4,10 +4,10 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require(__DIR__ . '/../vendor/yiisoft/yii2/framework/yii.php');
 
-Yii::$classMap['app\extensions\assetparser\Converter'] =__DIR__ . '/../vendor/yii_ext/assetparser/Converter.php';
-Yii::$classMap['app\extensions\assetparser\Parser'] =__DIR__ . '/../vendor/yii_ext/assetparser/Parser.php';
-Yii::$classMap['app\extensions\assetparser\Less'] =__DIR__ . '/../vendor/yii_ext/assetparser/Less.php';
-Yii::$classMap['app\extensions\assetparser\Sass'] =__DIR__ . '/../vendor/yii_ext/assetparser/Sass.php';
+Yii::$classMap['app\extensions\assetparser\Converter'] =__DIR__ . '/../vendor/yiiext/assetparser/Converter.php';
+Yii::$classMap['app\extensions\assetparser\Parser'] =__DIR__ . '/../vendor/yiiext/assetparser/Parser.php';
+Yii::$classMap['app\extensions\assetparser\Less'] =__DIR__ . '/../vendor/yiiext/assetparser/Less.php';
+Yii::$classMap['app\extensions\assetparser\Sass'] =__DIR__ . '/../vendor/yiiext/assetparser/Sass.php';
 
 $config = require(__DIR__ . '/../app/config/main.php');
 

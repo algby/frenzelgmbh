b94602dbd3a83d4b8b066fca6a6a10ae1ffd1130
Philipp Frenzel <philipp@frenzel.net>
2013-05-09 18:46:06 +0200

    switching to new structure

--- web/index.php
+++ web/index.php
@@ -2,16 +2,16 @@
 // comment out the following line to disable debug mode
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-$frameworkPath = __DIR__ . '/../vendor/yiisoft/yii2/framework';
+$frameworkPath = __DIR__ . '/../yii/vendor/yiisoft/yii2/framework';
 
 require($frameworkPath . '/Yii.php');
 // Register Composer autoloader
 @include($frameworkPath . '/vendor/autoload.php');
 
-Yii::$classMap['app\extensions\assetparser\Converter'] =__DIR__ . '/../vendor/yiiext/assetparser/Converter.php';
-Yii::$classMap['app\extensions\assetparser\Parser'] =__DIR__ . '/../vendor/yiiext/assetparser/Parser.php';
-Yii::$classMap['app\extensions\assetparser\Less'] =__DIR__ . '/../vendor/yiiext/assetparser/Less.php';
-Yii::$classMap['app\extensions\assetparser\Sass'] =__DIR__ . '/../vendor/yiiext/assetparser/Sass.php';
+Yii::$classMap['app\extensions\assetparser\Converter'] =__DIR__ . '/../yii/vendor/yiiext/assetparser/Converter.php';
+Yii::$classMap['app\extensions\assetparser\Parser'] =__DIR__ . '/../yii/vendor/yiiext/assetparser/Parser.php';
+Yii::$classMap['app\extensions\assetparser\Less'] =__DIR__ . '/../yii/vendor/yiiext/assetparser/Less.php';
+Yii::$classMap['app\extensions\assetparser\Sass'] =__DIR__ . '/../yii/vendor/yiiext/assetparser/Sass.php';
 
 $config = require(__DIR__ . '/../app/config/main.php');
 $application = new yii\web\Application($config);
